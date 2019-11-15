7d8100cfcc89b65a6238663dd7238f9f806a215b
MajidMoghadam2006 <majid.moghadam2006@gmail.com>
2019-11-15 02:20:51 -0800

    bash script added

---
+++ spec_test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+#PBS -l nodes=1:ppn=2 -l walltime=240:00:00
+#PBS -M renau@ucsc.edu
+ESESC_BIN=${1:-../main/esesc}
+export ESESC_ReportFile="report_exprtk_test"
+export ESESC_BenchName="./exprtk_test"
+export ESESC_nInstSkip=0
+export ESESC_nInstMax=2e9
+
+if [ -f $ESESC_BIN ]; then
+  $ESESC_BIN 
+else
+  $ESESC_BenchName 
+fi
+exit 0
